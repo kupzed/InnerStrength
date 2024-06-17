e220902a9ef70ecdae7f8f9d65c8cac9c97ba8a9
kupzed <rizafahdansyahda3@gmail.com>
2024-06-17 19:10:34 +0700

    MyProfile Update

--- src/pageslogin/profile/RiwayatPemeriksaan.jsx
+++ src/pageslogin/profile/RiwayatPemeriksaan.jsx
@@ -2,32 +2,53 @@ import React from 'react';
 import gracia from '../konsultasi/assets/gracia.jpg'
 import john from '../konsultasi/assets/john.png'
 import gladies from '../konsultasi/assets/gladies.png'
+import icondate from './assets/date.svg'
+import iconpackage from './assets/package.svg'
+import iconlike from './assets/likeblack.svg'
 
 const RiwayatPemeriksaan = () => {
   const riwayat = [
     {
-      id: 1,
-      nama: 'Gracia Stephanie S.Psi., M.Psi',
-      tanggal: '10 April 2024',
-      paket: 'Basic',
+      name: 'Gracia Stephanie S.Psi., M.Psi',
+      date: '10 April 2024',
+      package: 'Basic',
       status: 'Berlangsung',
-      image: gracia, // Ganti dengan path gambar yang sesuai
+      price: 'Rp. 100.000',
+      location: 'Komplek Cindirela, Medan Amplas',
+      mode: 'Online/Offline',
+      like: '70',
+      experience: '6',
+      expertise: ['Anxiety', 'Bipolar', 'Depresi', 'Hubungan dan keluarga'],
+      image: gracia,
+      description: 'Seorang psikolog berpengalaman dengan 6 tahun dedikasi dalam membantu individu dan keluarga mengatasi berbagai permasalahan psikologis, seperti kecemasan, bipolar, hubung...',
     },
     {
-      id: 2,
-      nama: 'John Alexander, S.Psi., M.Psi',
-      tanggal: '10 April 2024',
-      paket: 'Basic',
+      name: 'John Alexander, S.Psi., M.Psi',
+      date: '10 April 2024',
+      package: 'Basic',
       status: 'Selesai',
-      image: john, // Ganti dengan path gambar yang sesuai
+      price: 'Rp. 90.000',
+      location: 'Ruko Gardenia, Medan Patisah',
+      mode: 'Online/Offline',
+      like: '64',
+      experience: '5',
+      expertise: ['Depresi', 'Gangguan Kecemasan', 'Identitas Seksual', 'Gangguan Mood'],
+      image: john,
+      description: 'Seorang psikolog berpengalaman dengan 5 tahun dedikasi dalam membantu individu mengatasi berbagai permasalahan psikologis, seperti depresi, gangguan kecemasan, dan identitas seksual...',
     },
     {
-      id: 3,
-      nama: 'Gladies Anastasya, S.Psi., M.Psi',
-      tanggal: '10 April 2024',
-      paket: 'Basic',
+      name: 'Gladies Anastasya, S.Psi., M.Psi',
+      date: '10 April 2024',
+      package: 'Basic',
       status: 'Selesai',
-      image: gladies, // Ganti dengan path gambar yang sesuai
+      price: 'Rp. 100.000',
+      location: 'Simpang Kuda, Medan Tembung',
+      mode: 'Online/Offline',
+      like: '48',
+      experience: '5',
+      expertise: ['Bipolar', 'Trauma', 'Anxiety', 'Hubungan dan keluarga'],
+      image: gladies,
+      description: 'Seorang psikolog berpengalaman dengan 5 tahun dedikasi dalam membantu individu dan keluarga mengatasi berbagai permasalahan psikologis, seperti Bipolar, Trauma, Anxiety, Kelu...',
     },
   ];
 
@@ -39,19 +60,29 @@ const RiwayatPemeriksaan = () => {
           <div className="flex items-center">
             <img src={item.image} alt={item.nama} className="w-32 h-32 rounded mr-4" />
             <div className='text-lg'>
-              <h3 className="font-bold my-3">{item.nama}</h3>
-              <p className="text-gray-600 my-3">{item.tanggal}</p>
-              <p className="text-gray-600 my-3">{item.paket}</p>
+              <h3 className="font-bold my-3">{item.name}</h3>
+              <div className='inline-flex justify-normal items-center'>
+                  <img src={icondate} className='w-5 h-5 mr-2'></img>
+                  <p className="text-gray-600">{item.date}</p>
+                </div> <br />
+              <div className='inline-flex justify-normal items-center'>
+                  <img src={iconpackage} className='w-5 h-5 mr-2'></img>
+                  <p className="text-gray-600">{item.package}</p>
+                </div> <br />
+              <div className='inline-flex justify-normal items-center'>
+                  <img src={iconlike} className='w-5 h-5 mr-2'></img>
+                  <p className="text-gray-600">Suka</p>
+                </div>
             </div>
           </div>
           <div className="flex items-center space-x-4">
-            <span className={`px-2 py-1 rounded-full text-white ${item.status === 'Berlangsung' ? 'bg-green-500' : 'bg-gray-500'}`}>
+            <span className={`px-2 py-1 rounded-full text-white ${item.status === 'Berlangsung' ? 'bg-[#1572A1]' : 'bg-green-500'}`}>
               {item.status}
             </span>
             {item.status === 'Berlangsung' ? (
-              <button className="px-4 py-2 bg-blue-500 text-white rounded-md">Konsultasi</button>
+              <button className="px-4 py-2 bg-neutral-400 text-white rounded-md">Konsultasi</button>
             ) : (
-              <button className="px-4 py-2 bg-blue-500 text-white rounded-md">Pesan Kembali</button>
+              <button className="px-4 py-2 bg-neutral-400 text-white rounded-md">Pesan Kembali</button>
             )}
           </div>
         </div>
